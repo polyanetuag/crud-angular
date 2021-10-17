24a3c6aec7130e266a78846b6c545ffe99a34614
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-17 14:45:18 -0300

    navigation routes for product creation

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProductService } from "../product.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-create',
-  templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  selector: "app-product-create",
+  templateUrl: "./product-create.component.html",
+  styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
+  constructor(private productService: ProductService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  createProduct(): void {
+    this.productService.showMessage("Produto criado!");
   }
 
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
 }
