5a4f150683b3ea601fe3f8de737489a8025b50fa
Polyane Tuag <pollyportugues@hotmail.com>
2021-10-30 11:09:24 -0300

    understanding generated component

--- frontend/src/app/components/product/product-read2/product-read2-datasource.ts
+++ frontend/src/app/components/product/product-read2/product-read2-datasource.ts
@@ -1,37 +1,32 @@
-import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { map } from 'rxjs/operators';
-import { Observable, of as observableOf, merge } from 'rxjs';
-
-// TODO: Replace this with your own data model type
-export interface ProductRead2Item {
-  name: string;
-  id: number;
-}
+import { DataSource } from "@angular/cdk/collections";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatSort } from "@angular/material/sort";
+import { map } from "rxjs/operators";
+import { Observable, of as observableOf, merge } from "rxjs";
+import { Product } from "../product.model";
 
 // TODO: replace this with real data from your application
-const EXAMPLE_DATA: ProductRead2Item[] = [
-  {id: 1, name: 'Hydrogen'},
-  {id: 2, name: 'Helium'},
-  {id: 3, name: 'Lithium'},
-  {id: 4, name: 'Beryllium'},
-  {id: 5, name: 'Boron'},
-  {id: 6, name: 'Carbon'},
-  {id: 7, name: 'Nitrogen'},
-  {id: 8, name: 'Oxygen'},
-  {id: 9, name: 'Fluorine'},
-  {id: 10, name: 'Neon'},
-  {id: 11, name: 'Sodium'},
-  {id: 12, name: 'Magnesium'},
-  {id: 13, name: 'Aluminum'},
-  {id: 14, name: 'Silicon'},
-  {id: 15, name: 'Phosphorus'},
-  {id: 16, name: 'Sulfur'},
-  {id: 17, name: 'Chlorine'},
-  {id: 18, name: 'Argon'},
-  {id: 19, name: 'Potassium'},
-  {id: 20, name: 'Calcium'},
+const EXAMPLE_DATA: Product[] = [
+  { id: 1, name: "Hydrogen" },
+  { id: 2, name: "Helium" },
+  { id: 3, name: "Lithium" },
+  { id: 4, name: "Beryllium" },
+  { id: 5, name: "Boron" },
+  { id: 6, name: "Carbon" },
+  { id: 7, name: "Nitrogen" },
+  { id: 8, name: "Oxygen" },
+  { id: 9, name: "Fluorine" },
+  { id: 10, name: "Neon" },
+  { id: 11, name: "Sodium" },
+  { id: 12, name: "Magnesium" },
+  { id: 13, name: "Aluminum" },
+  { id: 14, name: "Silicon" },
+  { id: 15, name: "Phosphorus" },
+  { id: 16, name: "Sulfur" },
+  { id: 17, name: "Chlorine" },
+  { id: 18, name: "Argon" },
+  { id: 19, name: "Potassium" },
+  { id: 20, name: "Calcium" },
 ];
 
 /**
@@ -39,8 +34,8 @@ const EXAMPLE_DATA: ProductRead2Item[] = [
  * encapsulate all logic for fetching and manipulating the displayed data
  * (including sorting, pagination, and filtering).
  */
-export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
-  data: ProductRead2Item[] = EXAMPLE_DATA;
+export class ProductRead2DataSource extends DataSource<Product> {
+  data: Product[] = EXAMPLE_DATA;
   paginator: MatPaginator | undefined;
   sort: MatSort | undefined;
 
@@ -53,16 +48,23 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
    * the returned stream emits new items.
    * @returns A stream of the items to be rendered.
    */
-  connect(): Observable<ProductRead2Item[]> {
+  connect(): Observable<Product[]> {
     if (this.paginator && this.sort) {
       // Combine everything that affects the rendered data into one update
       // stream for the data-table to consume.
-      return merge(observableOf(this.data), this.paginator.page, this.sort.sortChange)
-        .pipe(map(() => {
-          return this.getPagedData(this.getSortedData([...this.data ]));
-        }));
+      return merge(
+        observableOf(this.data),
+        this.paginator.page,
+        this.sort.sortChange
+      ).pipe(
+        map(() => {
+          return this.getPagedData(this.getSortedData([...this.data]));
+        })
+      );
     } else {
-      throw Error('Please set the paginator and sort on the data source before connecting.');
+      throw Error(
+        "Please set the paginator and sort on the data source before connecting."
+      );
     }
   }
 
@@ -76,7 +78,7 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
    * Paginate the data (client-side). If you're using server-side pagination,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getPagedData(data: ProductRead2Item[]): ProductRead2Item[] {
+  private getPagedData(data: Product[]): Product[] {
     if (this.paginator) {
       const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
       return data.splice(startIndex, this.paginator.pageSize);
@@ -89,23 +91,30 @@ export class ProductRead2DataSource extends DataSource<ProductRead2Item> {
    * Sort the data (client-side). If you're using server-side sorting,
    * this would be replaced by requesting the appropriate data from the server.
    */
-  private getSortedData(data: ProductRead2Item[]): ProductRead2Item[] {
-    if (!this.sort || !this.sort.active || this.sort.direction === '') {
+  private getSortedData(data: Product[]): Product[] {
+    if (!this.sort || !this.sort.active || this.sort.direction === "") {
       return data;
     }
 
     return data.sort((a, b) => {
-      const isAsc = this.sort?.direction === 'asc';
+      const isAsc = this.sort?.direction === "asc";
       switch (this.sort?.active) {
-        case 'name': return compare(a.name, b.name, isAsc);
-        case 'id': return compare(+a.id, +b.id, isAsc);
-        default: return 0;
+        case "name":
+          return compare(a.name, b.name, isAsc);
+        case "id":
+        // return compare(+a.id, +b.id, isAsc);
+        default:
+          return 0;
       }
     });
   }
 }
 
 /** Simple sort comparator for example ID/Name columns (for client-side sorting). */
-function compare(a: string | number, b: string | number, isAsc: boolean): number {
+function compare(
+  a: string | number,
+  b: string | number,
+  isAsc: boolean
+): number {
   return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
