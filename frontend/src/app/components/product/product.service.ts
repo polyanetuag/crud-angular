d276824b45a45ea2b1f50a87c7d65a930c29a3a3
Polyane Tuag <pollyportugues@hotmail.com>
2021-11-02 16:03:32 -0300

    displaying error messages

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
-import { Observable } from "rxjs";
+import { EMPTY, Observable } from "rxjs";
+import { catchError, map } from "rxjs/operators";
 import { Product } from "./product.model";
 
 @Injectable({
@@ -12,16 +13,25 @@ export class ProductService {
 
   constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, "X", {
       duration: 3000,
       horizontalPosition: "right",
       verticalPosition: "top",
+      panelClass: isError ? ["msg-error"] : ["msg-success"],
     });
   }
   //interação com o backend - criação dos produtos
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product);
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage("Ocorreu um erro!", true);
+    return EMPTY;
   }
 
   // leitura dos produtos
